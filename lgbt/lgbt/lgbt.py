b99c7939b4da9bc8c87c8976f8d20c5c6dca2fbb
Kopylov Matvey <kopylovmatveyx@gmail.com>
2025-03-28 09:15:29 +0700

    v 0.2.0

--- lgbt/lgbt/lgbt.py
+++ lgbt/lgbt/lgbt.py
@@ -1,8 +1,10 @@
 import time
 import sys
-from tqdm import tqdm
 
-def desc_prep(desc):
+colours = ["\033[31m", "\033[38;5;214m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m" ]
+heroes = {'rainbow': '🌈', 'unicorn':'🦄', 'teddy': '🧸', 'bunny': '🐰', 'kitten':'🐱', 'sakura':'🌸', 'heart':'🩷'}
+
+def desc_prep(desc, hero):
 	"""
 	Formating description string if it's too long
 	"""
@@ -11,27 +13,34 @@ def desc_prep(desc):
 		new_desc = desc[:9] + "... " 
 	else:
 		new_desc = desc + (" " * (12-length))
-	return "🌈" + new_desc + ":"
+	return heroes[hero] + new_desc + ":"
 
-def lgbt(iterable, desc=" ", miniters=2500, placeholder='▋'):
+def fill_bars(bar_width, placeholder):
+	free_spaces = " " * 8
+	bar = colours[0] + free_spaces + colours[1] + free_spaces + colours[2] + free_spaces + colours[3] + free_spaces + colours[4] + free_spaces + colours[5] + free_spaces + colours[6] + free_spaces
+	bars = []
+	for i in range(0, bar_width+1):
+		bars.append(bar.replace(' ', placeholder, i))
+	return bars
+
+def lgbt(iterable, desc=" ", miniters=2500, placeholder='▋', hero='rainbow'):
 	"""
 	Progress bar
 	iterable    - list of elements
 	desc        - description
 	miniters    - minimal iterations between update screen
 	placeholder - symbol which used in progress bar 
+	hero        - сhoose your smiley face
 	"""
-	colours = ["\033[31m", "\033[38;5;214m", "\033[33m", "\033[32m", "\033[36m", "\033[34m", "\033[35m" ]
-	
-	desc = desc_prep(desc)
+
+	desc = desc_prep(desc, hero)
 	number_of_colours = len(colours)
 	total = len(iterable)
 	bar_width = 56  
 	step = bar_width // number_of_colours
 	miniters = max(1, total/miniters)
-
-	free_spaces = " " * 8
-	bar = colours[0] + free_spaces + colours[1] + free_spaces + colours[2] + free_spaces + colours[3] + free_spaces + colours[4] + free_spaces + colours[5] + free_spaces + colours[6] + free_spaces
+	
+	bars = fill_bars(bar_width=bar_width, placeholder=placeholder)
 	
 	start = time.perf_counter()
 	for i, data in enumerate(iterable, 1):
@@ -45,9 +54,5 @@ def lgbt(iterable, desc=" ", miniters=2500, placeholder='▋'):
 			percent = (i / total) * 100  
 
 			sys.stdout.write(
-				f"\r{desc}{current_colour}{percent:03.0f}% {bar.replace(' ', placeholder, filled)}{current_colour}[{i}/{total}] [{end:.2f}s, {i/end:.2f}it/s]  \033[m")
+				f"\r{desc}{current_colour}{percent:03.0f}% {bars[filled]}{current_colour}[{i}/{total}] [{end:.2f}s, {i/end:.2f}it/s]  \033[m")
 			sys.stdout.flush()
-	
-for i in lgbt(range(100)):
-	time.sleep(0.1)
-	j = 0
