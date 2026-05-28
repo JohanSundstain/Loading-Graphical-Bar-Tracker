b81eb9dd9722f0a41b0bbd9f600b53839a6e2dd8
johansundstain <johansundstain@outlook.com>
2026-05-28 20:06:05 +0700

    Update 3.0.0 with bug fixing

---
+++ lgbt_project/lgbt/bar.py
@@ -0,0 +1,135 @@
+from abc import abstractmethod
+import time
+import sys
+import os
+from consts import FLAGS, HAND_KEYS
+
+class ConsoleObject():
+	def __init__(self, coord=(1,1)):
+		self._coord = coord
+		self._time = None
+		self._value = None
+		self._buffer = []
+
+	@property
+	def time(self):
+		return self._time
+	
+	@time.setter
+	def time(self, value):
+		self._time = value
+
+	@property
+	def coord(self):
+		return self._coord
+	
+	@coord.setter
+	def coord(self, value):
+		self._coord = value
+
+	@abstractmethod
+	def draw(self):
+		pass
+
+	@abstractmethod
+	def update(self, value):
+		if self._time == None:
+			self._time = time.perf_counter()
+		self._value = value
+		
+	def cursor(self, column, row):
+		self._buffer.append(f"\033[{row};{column}H")
+
+	def put_in_buffer(self, value):
+		self._buffer.append(value)
+
+	def flush(self, buffer=None):
+		if buffer == None:
+			sys.stdout.write("".join(self._buffer))
+			sys.stdout.flush()
+			self._buffer.clear()
+		else:
+			buffer.extend(self._buffer)
+			self._buffer.clear()
+		
+	
+class Bar(ConsoleObject):
+	def __init__(self, desc, total, mode="rainbow", fixed=False):
+		if fixed:
+			os.system('cls' if os.name == 'nt' else 'clear')  	
+
+		super(Bar, self).__init__()
+		self._bar = FLAGS[mode].split(HAND_KEYS['RESET'])
+		self._part_bars = []
+
+		self._start_time = None
+		self._value = None
+		self._max_desc = 10
+		self._total = total
+		self._width = 63
+		self._desc = self._check_desc(desc),
+
+		self._stats = {
+			"percent" : 		0.0,
+			"filled" : 			0,
+			'passed_time':		0.0,
+			'remaining_time' : 	0.0,
+			'speed' : 			0.0,
+			'current_iter': 	0,
+			'total_iter' : 		total
+			}
+		
+		self._fill_bar()
+	
+	def _fill_bar(self):
+		n = len(self._bar)
+		curr_str = ""
+		for i, simb in enumerate(self._bar, 1):
+			curr_str += simb
+			self._part_bars.append((curr_str + HAND_KEYS['RESET']) + (" " * (n - i)) )
+
+	def _translate_count(self, iter):
+		if iter >= 1000000:
+			return f'{iter/1000000:.0f}M'
+		if iter >= 1000:
+			return f'{iter/1000:.0f}K'
+		return f'{iter:.0f}'
+	
+	def _translate_time(self, sec):
+		total_seconds = int(sec)
+		if total_seconds > 3600:
+			hours = total_seconds // 3600
+			remaining_seconds = total_seconds % 3600
+			minutes = remaining_seconds // 60
+			seconds = remaining_seconds % 60
+			return f'{hours}:{minutes:02}:{seconds:02}'
+		else:
+			seconds = total_seconds % 60
+			minutes = total_seconds // 60
+			return f'{minutes:02}:{seconds:02}'
+		
+	def _check_desc(self, desc):
+		if len(desc) > self._max_desc:
+			desc = desc[:(self._max_desc-3)]
+			desc += "..."
+		return desc
+	
+	def update(self, value):
+		super().update(value)
+		self._stats['percent'] = (self._value / self._total) * 100  
+		self._stats['filled']  = round(self._value / self._total * (len(self._bar)-1))
+		self._stats['passed_time'] = time.perf_counter() - self._time
+		self._stats['speed'] = self._value / self._stats['passed_time']
+		self._stats['remaining_time'] = (self._total - self._value) / self._stats['speed'] 
+		self._stats['current_iter'] = self._value
+
+	def draw(self):
+		desc = self._desc
+		percent = self._stats['percent']
+		filled = self._stats['filled']
+		passed_time = self._translate_time(self._stats['passed_time'])
+		remaining_time = self._translate_time(self._stats['remaining_time'])
+		speed =  self._translate_count(self._stats['speed'])
+		current_iter = self._translate_count(self._stats['current_iter'])
+		total = self._translate_count(self._stats['total_iter'])
+		self.put_in_buffer(f"\r{desc[0]} {percent:03.0f}% {self._part_bars[filled]} {current_iter}/{total} [{passed_time}<{remaining_time}, {speed}it/s]{HAND_KEYS['CLEAN']}")
--- lgbt_project/lgbt/consts.py
+++ lgbt_project/lgbt/consts.py
@@ -1,6 +1,5 @@
 PH = '█'
 BIG=63
-SHORT=21
 
 # set column and row
 def cursor(column, row):
@@ -49,26 +48,6 @@ BACHGROUND = {
 		'WHITE':"\033[47m"
 		}
 
-HEROES = {
-		'rainbow': '🌈',
-		'unicorn':'🦄',
-		'teddy': '🧸',
-		'bunny': '🐰',
-		'kitten':'🐱',
-		'sakura':'🌸',
-		'heart':'💖',
-		'gonechar':'🐝',
-		'tralalero':'🦈',
-		'crocodillo': '🐊',
-		'tumtumtum': '🗿',
-		'shimpanzini': '🍌',
-		'trippi':'🦐',
-		'goozinni':'🪿',
-		'gpu': '🎮',
-		'cpu': '🧠',
-		'histogram': '📊'
-		}
-
 COLS = {0.0: ' ', 0.125: '▁', 0.25 : '▂', 0.375: '▃', 0.5: '▄', 0.625: '▅', 0.75 : '▆', 0.875: '▇' , 1.0: '█' }
 COLS_KEYS = list(COLS.keys())
 
@@ -82,18 +61,6 @@ def paint(str, color, count, background=None):
 	return f"{COLOURS[color]}{str}{HAND_KEYS['RESET']}" * count
 
 
-def rotate_left(s, n):
-    n = n % len(s)  
-    return s[n:] + s[:n]
-
-def create_string_anim(str):
-	ext_str = str + (" " * len(str))
-	anim = []
-	for i in range(len(ext_str)):
-		anim.append(rotate_left(ext_str, i))
-	
-	return anim
-
 def repeat(str, count):
 	return str * count
 
@@ -104,7 +71,7 @@ def upper_bound(value):
 			return COLS[COLS_KEYS[i-1]]
 
 # 63 length all strings
-BIG_FLAGS =  {
+FLAGS =  {
 			'white' : paint(PH, 'WHITE', 63),
 			'rainbow': paint(PH,'RED', 9) + paint(PH,'ORANGE', 9) + paint(PH,'YELLOW', 9) + paint(PH,'GREEN', 9) + paint(PH,'BRIGHT BLUE', 9) + paint(PH, 'BLUE', 9) + paint(PH, 'PURPLE', 9),
 			'usa': repeat(paint(PH,'BLUE', 1) + paint('⋆','WHITE', 1, 'BLUE'), 10)  + paint(PH,'BLUE', 1) + repeat(paint(PH,'RED',1) + paint(PH,'WHITE',1), 21),
@@ -125,32 +92,4 @@ BIG_FLAGS =  {
 
 			'eng': paint('━', 'RED',   31, 'WHITE') + paint('╋','RED',     1, 'WHITE')  + paint('━', 'RED', 31,'WHITE'),
 			'jpn': paint(PH,'WHITE', 31)            + paint('●','RED',     1, 'WHITE')  + paint(PH,'WHITE', 31),
-			}
-
-# 21 length all strings
-SHORT_FLAGS =  {
-			'white' : paint(PH,'WHITE', 21),
-			'rainbow': paint(PH,'RED', 3) + paint(PH,'ORANGE', 3) + paint(PH,'YELLOW', 3) + paint(PH,'GREEN', 3) + paint(PH,'BRIGHT BLUE', 3) + paint(PH, 'BLUE', 3) + paint(PH, 'PURPLE', 3),
-			'usa': repeat(paint(PH,'BLUE', 1) + paint('⋆','WHITE', 1, 'BLUE'), 4)  + paint(PH,'BLUE', 1) + repeat(paint(PH,'RED',1) + paint(PH,'WHITE',1), 6),
-			'chn': paint(PH,'RED', 1) + paint('★ ','YELLOW', 1, 'RED') + repeat(paint(PH,'RED',1) + paint('⭑','YELLOW', 1,'RED'), 3) + paint(PH, 'RED', 12),
-
-			'ussr':paint(PH,'RED',  1) + paint('☭ ','YELLOW', 1, 'RED') + paint(PH,'RED', 18),
-
-			'rus': paint(PH,'WHITE', 7) + paint(PH,'BLUE',   7) + paint(PH,'RED',    7), 
-			'ita': paint(PH,'GREEN', 7) + paint(PH,'WHITE',  7) + paint(PH,'RED',    7), 
-			'rue': paint(PH,'BLACK', 7) + paint(PH,'YELLOW', 7) + paint(PH,'WHITE',  7),
-			'deu': paint(PH,'BLACK', 7) + paint(PH,'RED',    7) + paint(PH,'ORANGE', 7),
-			'fra': paint(PH,'BLUE',  7) + paint(PH,'WHITE',  7) + paint(PH,'RED',    7),
-
-			'swe': paint('━', 'YELLOW', 3,'BRIGHT BLUE') + paint('╋','YELLOW', 1, 'BRIGHT BLUE') + paint('━', 'YELLOW', 17,'BRIGHT BLUE'),
-			'fin': paint('━', 'BLUE',   3,'WHITE')       + paint('╋','BLUE',   1, 'WHITE')       + paint('━', 'BLUE',   17,'WHITE'),
-			'nor': paint('━', 'BLUE',   3 ,'RED')         + paint('╋','BLUE',  1, 'RED')         + paint('━', 'BLUE',   17,'RED'),
-			'dnk': paint('━', 'WHITE',  3,'RED')          + paint('╋','WHITE', 1, 'RED')         + paint('━', 'WHITE',  17,'RED'),
-
-			'eng': paint('━', 'RED',   10, 'WHITE')   + paint('╋','RED',       1, 'WHITE')  + paint('━', 'RED', 10,'WHITE'),
-			'jpn': paint(PH,'WHITE',   10)            + paint('●','RED',       1, 'WHITE')  + paint(PH,'WHITE', 10),
-
-			'intel' : paint('I', 'BLUE', 1, 'WHITE') + paint('n', 'BLUE', 1, 'WHITE') + paint('t', 'BLUE', 1, 'WHITE')+ paint('e', 'BLUE', 1, 'WHITE')+ paint('L', 'BLUE', 1, 'WHITE') + paint(PH, 'BLUE', 16, 'WHITE'),
-			'amd' : paint('A', 'WHITE', 1, 'RED') +paint('M', 'WHITE', 1, 'RED') +paint('D', 'WHITE', 1, 'RED') + paint('◥', 'WHITE', 1, 'RED') + paint(PH, 'RED', 17),
-			'nvidia': paint('᪤', 'GREEN', 1, 'BLACK') + paint(PH, 'BLACK', 1)+ paint('n', 'WHITE', 1, 'BLACK')+ paint('v', 'WHITE', 1, 'BLACK')+ paint('i', 'WHITE', 1, 'BLACK')+ paint('d', 'WHITE', 1, 'BLACK')+ paint('i', 'WHITE', 1, 'BLACK')+ paint('a', 'WHITE', 1, 'BLACK')+ paint(PH, 'BLACK', 13)
 			}
